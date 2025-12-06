b89c86b90d8b1b1e2399563549751f04ddcc2d91
Aline Baudry <aline.baudry@sorbonne-universite.fr>
2025-12-06 11:40:08 +0100

    day3: star1

--- day3/main.cpp
+++ day3/main.cpp
@@ -1,37 +1,55 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>     /* abs */
-#include <map>
+#include <vector>
 #include <string>
+#include <limits>
 
 using namespace std;
 
 int main()
 {
-    int res;
+    int res = 0;
     string str;
 
     // open puzzle input
-    ifstream f("input_test.txt");
+    ifstream f("input.txt");
     if (!f.is_open()){
         cerr << "Error opening the file!";
         return 1;
     }
 
-    while (getline(f, str)){  
-        map<int, int> m;
-        for (char c : str){
-            auto val = static_cast<int>(c-'0');
-            
-        }
+    while (getline(f, str)){
+        
+        std::vector<int>::iterator result;
+        int joltage;
 
+        // extract digits
+        vector<int> v;
+        for (char c : str) v.push_back(static_cast<int>(c-'0'));
+
+        // find first digit of joltage rating, i.e. max in line
+        result = std::max_element(
+            v.begin(), 
+            v.end()-1
+        );
+        joltage = *result * 10;
+
+        // find second digit of voltage rating, i.e. max in rest of line
+        result = std::max_element(
+            v.begin() + std::distance(v.begin(), result) + 1,
+            v.end()
+        );
+        joltage += *result;
+
+        // std:: cout << "joltage :"
+        //         << joltage << endl;
+
+        res+=joltage;
     }
     f.close();
 
-
-
     // compute and display the result
-    res = 0;
     cout << "res: " << res << endl;
 
     return 0;

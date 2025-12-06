3a02525249a40d1b1bf4008e926da347cf1f1fbc
Aline Baudry <aline.baudry@sorbonne-universite.fr>
2025-12-06 11:56:50 +0100

    day3: change algorithm for part2, still works on part1

--- day3/main.cpp
+++ day3/main.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>     /* abs */
 #include <vector>
 #include <string>
-#include <limits>
+#include <cmath>
 
 using namespace std;
 
@@ -13,7 +13,7 @@ int main()
     string str;
 
     // open puzzle input
-    ifstream f("input.txt");
+    ifstream f("input_test.txt");
     if (!f.is_open()){
         cerr << "Error opening the file!";
         return 1;
@@ -22,35 +22,38 @@ int main()
     while (getline(f, str)){
         
         std::vector<int>::iterator result;
-        int joltage;
+        int joltage = 0;
+        int nb_batteries = 2;       // part 1
 
         // extract digits
         vector<int> v;
         for (char c : str) v.push_back(static_cast<int>(c-'0'));
 
-        // find first digit of joltage rating, i.e. max in line
-        result = std::max_element(
-            v.begin(), 
-            v.end()-1
-        );
-        joltage = *result * 10;
+        cout << "---\n" << str << endl;
 
-        // find second digit of voltage rating, i.e. max in rest of line
-        result = std::max_element(
-            v.begin() + std::distance(v.begin(), result) + 1,
-            v.end()
-        );
-        joltage += *result;
+        for (int i=1; i<=nb_batteries; i++){
 
-        // std:: cout << "joltage :"
-        //         << joltage << endl;
+            for (auto it : v) cout << it << ", ";
+            cout << endl;
+            
+            result = std::max_element(
+                v.begin(), 
+                v.end()-(nb_batteries-i)
+            );
+            
+            std::cout << "Max element found at index "
+                    << std::distance(v.begin(), result)
+                    << " has value " << *result << '\n';
 
-        res+=joltage;
+            joltage += *result * pow(10, nb_batteries-i);
+
+            cout << "joltage: " << joltage << endl; 
+
+            v.erase(v.begin(), v.begin() + std::distance(v.begin(), result) + 1);
+        }
+        res += joltage;
     }
     f.close();
-
-    // compute and display the result
     cout << "res: " << res << endl;
-
     return 0;
 }

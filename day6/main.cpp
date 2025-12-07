8c5ae181e4837d8a617f79ca4799b8ce9953632c
Aline Baudry <aline.baudry@sorbonne-universite.fr>
2025-12-07 20:38:02 +0100

    day6: parsing the file to extract numbers in colums (C-style I/O)

--- day6/main.cpp
+++ day6/main.cpp
@@ -18,8 +18,11 @@ int main()
 {
     // puzzle variables
     vector<operation> operations;
-    vector<operation> cephalopod_operations;
     int operation_index;
+    string str;
+
+    bool part_1 = false;
+    bool part_2 = true;
 
     // open puzzle input
     ifstream f("input_test.txt");
@@ -29,76 +32,102 @@ int main()
     }
 
     // extract data from puzzle input ----------
-    string str;
-    regex number_regex("\\d+");
-    regex operation_regex("\[+*]");
-    smatch range_match;
-    sregex_iterator str_begin, str_end;
+    
+    // for part. 1
+    if (part_1){
 
-    bool first_line = true;
+        regex number_regex("\\d+");
+        regex operation_regex("\[+*]");
+        smatch range_match;
+        sregex_iterator str_begin, str_end;
 
-    while (getline(f, str)){
+        bool first_line = true;
 
-        // number ?
-        operation_index = 0;
-        str_begin = sregex_iterator(str.begin(), str.end(), number_regex);
-        str_end = sregex_iterator();
+        while (getline(f, str)){
 
-        for (sregex_iterator i = str_begin; i != str_end; i++) {
-            
-            smatch match = *i;
-            string match_str = match.str();
+            // number ?
+            operation_index = 0;
+            str_begin = sregex_iterator(str.begin(), str.end(), number_regex);
+            str_end = sregex_iterator();
 
-            if (first_line) {
-                vector<int> numbers;
-                numbers.push_back(stoi(match_str));
-                operation operation_tmp{numbers, '.'};
-                operations.push_back(operation_tmp);
+            for (sregex_iterator i = str_begin; i != str_end; i++) {
+                
+                smatch match = *i;
+                string match_str = match.str();
+
+                if (first_line) {
+                    vector<int> numbers;
+                    numbers.push_back(stoi(match_str));
+                    operation operation_tmp{numbers, '.'};
+                    operations.push_back(operation_tmp);
+                }
+                else {
+                    operations[operation_index].numbers.push_back(stoi(match_str));
+                }
+                operation_index++;
             }
-            else {
-                operations[operation_index].numbers.push_back(stoi(match_str));
+            first_line = false;
+
+            // if not number, look for operation sign
+            operation_index = 0;
+            str_begin = sregex_iterator(str.begin(), str.end(), operation_regex);
+            str_end = sregex_iterator();
+
+            for (sregex_iterator i = str_begin; i != str_end; i++) {
+                
+                smatch match = *i;
+                string match_str = match.str();
+                operations[operation_index].type = match_str[0];
+                operation_index++;
             }
-            operation_index++;
         }
-        first_line = false;
+    } // part. 1
 
-        // if not number, look for operation sign
-        operation_index = 0;
-        str_begin = sregex_iterator(str.begin(), str.end(), operation_regex);
-        str_end = sregex_iterator();
-
-        for (sregex_iterator i = str_begin; i != str_end; i++) {
-            
-            smatch match = *i;
-            string match_str = match.str();
-            operations[operation_index].type = match_str[0];
-            operation_index++;
-        }
-    }
-    f.close();
-
+    // for part. 2
     // Cephalopod math is written right-to-left in columns ----------
-    for (auto it : operations){
-        int nb_digits = 0;
+    // we need to parse the file another way
+    // 2. then parse the file column by column
+    if (part_2){
 
-        for (auto n : it.numbers) cout << n << ", ";
-        cout << endl;
-
-        // we need to know how many digits we've got to synchronize colums
-        // example :
-        //
-        for (auto n : it.numbers){
-            nb_digits = (floor(log10(n))+1 > nb_digits) ? floor(log10(n))+1 : nb_digits;
+        // 1. get number of rows and cols
+        int nb_cols = 0;
+        int nb_rows = 0;
+        while (getline(f, str)){
+            cout << str << endl;
+            nb_cols = str.length();
+            nb_rows++;
         }
+        f.close();
 
+        cout << "nb_rows: " << nb_rows << ", nb_cols: " << nb_cols << endl;
+
+        // 2. then parse the file column by column to get numbers
+        int col = 0;
+        char c;
         vector<int> numbers;
-        for (auto n : it.numbers){
 
-            int corrected_number = n * pow(10, nb_digits-floor(log10(n)+1));
-            cout << n << " --> " << corrected_number << endl;
+        FILE* fp = std::fopen("input_test.txt", "rb");  // we need to open the file in binary mode
+
+        while (col < nb_cols) {
+
+            vector<int> digits;
+
+            for (int row = 0; row <nb_rows-1; row++){
+
+                // get char of col line by line
+                int offset = row*(nb_cols+1)+col;
+                fseek(fp, offset, SEEK_SET);        // go at the position of the interesting char in file
+                c = fgetc(fp);
+
+                int digit = static_cast<int>(c-'0');
+                if ( digit>=0 && digit>=9 ) digits.push_back(digit);
+
+                cout << "offset: " << offset << ", c: " << c << ", digit: " << digit << endl;
+            }
+            col++;
         }
+        fclose(fp);
     }
-
 
     // display operations ----------
     cout << "---\noperations:\n";
@@ -107,14 +136,8 @@ int main()
         for (auto n : it.numbers) cout << n << ", ";
         cout << endl;
     }
-    cout << "---\ncephalopod operations:\n";
-    for (auto it : cephalopod_operations){
-        cout << it.type << ": ";
-        for (auto n : it.numbers) cout << n << ", ";
-        cout << endl;
-    }
 
-    // part 1 ----------
+    // compute result ----------
     long int res_1 = 0;
     for (auto it : operations){
 
@@ -129,11 +152,6 @@ int main()
         res_1 += res;
     }
 
-    // part 2 ----------
-    int res_2 = 0;
-
-
-    cout << "part. 1: " << res_1 << endl;
-    cout << "part. 2: " << res_2 << endl;
+    cout << "res : " << res_1 << endl;
     return 0;
 }

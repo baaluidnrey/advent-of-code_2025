10dd10347644a4a7ca9b23bc6016168be725bae5
Aline Baudry <aline.baudry@sorbonne-universite.fr>
2025-12-07 14:50:31 +0100

    day5: concatenation of ranges OK

--- day5/main.cpp
+++ day5/main.cpp
@@ -14,6 +14,64 @@ struct range {
     long int max;
 };
 
+int ConcatenateRanges(vector<range> input, vector<range> &output, bool debug){
+
+    output.push_back(input[0]);
+    range range_tmp;
+    int nb_concatenation = 0;
+
+    for (auto range : input){
+
+        if (debug){
+            cout << "---\nrange: (" << range.min << "," << range.max << ")\n";
+            cout << "corrected ranges:\n";
+            for (auto it : output) cout << "(" << it.min << ", " << it.max << "), ";
+            cout << endl;
+        }
+
+        bool range_to_add = true;
+
+        for (int j=0; j<output.size(); j++){
+            auto corrected_range = output.at(j);
+
+            bool superposition = false;
+            auto min = corrected_range.min;
+            auto max = corrected_range.max;
+
+            if (debug) cout << "corr_range: (" << corrected_range.min << "," << corrected_range.max << ")\t";
+
+            if (range.min == min && range.max == max){       // same range, do nothing
+                if (debug) cout << "same range\n";
+                range_to_add = false;
+            }
+            
+            else{
+
+                if (range.min < min && range.max >= min){   // superposition from bottom
+                    min = range.min;
+                    superposition = true;
+                }
+
+                if (range.min <= max && range.max > max){   // superposition from top
+                    max = range.max;
+                    superposition = true;
+                }
+
+                range_tmp.min = min;
+                range_tmp.max = max;
+                if (superposition){
+                    output.at(j) = range_tmp;
+                    range_to_add = false;
+                    nb_concatenation++;
+                }              
+                if (debug) cout << "--> (" << min << "," << max << ")\n";
+            }
+        }
+        if (range_to_add)     output.push_back(range);
+    }
+    return nb_concatenation;
+}
+
 int main()
 {
     // puzzle variables
@@ -38,6 +96,7 @@ int main()
 
     while (getline(f, str)){  
 
+        // ingredient ?
         if ( regex_match(str, ingredient_regex) ){
             v_ingredient.push_back(stol(str));
         }
@@ -96,56 +155,17 @@ int main()
     // }
     // res = fresh_ingredients.size();
 
-    v_corrected_ranges.push_back(v_ranges[0]);
-    range range_tmp;
-
-    for (auto range : v_ranges){
-
-        cout << "---\nrange: (" << range.min << "," << range.max << ")\n";
-
-        cout << "corrected ranges:\n";
-        for (auto it : v_corrected_ranges) cout << "(" << it.min << ", " << it.max << "), ";
-        cout << endl;
-
-        bool range_to_add = true;
-
-        for (int j=0; j<v_corrected_ranges.size(); j++){
-            auto corrected_range = v_corrected_ranges.at(j);
-
-            bool superposition = false;
-            auto min = corrected_range.min;
-            auto max = corrected_range.max;
-
-            cout << "corr_range: (" << corrected_range.min << "," << corrected_range.max << ")\t";
-
-            if (range.min == min && range.max == max){       // same range, do nothing
-                cout << "same range\n";
-                range_to_add = false;
-            }
-            
-            else{
-
-                if (range.min < min && range.max >= min){   // superposition from bottom
-                    min = range.min;
-                    superposition = true;
-                }
-
-                if (range.min <= max && range.max > max){   // superposition from top
-                    max = range.max;
-                    superposition = true;
-                }
-
-                range_tmp.min = min;
-                range_tmp.max = max;
-                if (superposition){
-                    v_corrected_ranges.at(j) = range_tmp;
-                    range_to_add = false;
-                }              
-                cout << "--> (" << min << "," << max << ")\n";
-            }
-        }
-        if (range_to_add)     v_corrected_ranges.push_back(range);
+    // concatenation of ranges ----------
+    int nb_concatenations;
+    vector<range> v_ranges_tmp = v_ranges;
+    do{
+        v_corrected_ranges.clear();
+        nb_concatenations = ConcatenateRanges(v_ranges_tmp, v_corrected_ranges, false);
+        v_ranges_tmp = v_corrected_ranges;
+        cout << "nb_concatenations: " << nb_concatenations << endl;
     }
+    while (nb_concatenations!=0);
+
 
     cout << "corrected ranges:\n";
     for (auto it : v_corrected_ranges) cout << "(" << it.min << ", " << it.max << "), ";
